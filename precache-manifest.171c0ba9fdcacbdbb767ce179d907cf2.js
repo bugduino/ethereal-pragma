d8898b7e324e97eff042850525ef48d66b48158c
bugman <onlyforwin@gmail.com>
2019-05-01 01:11:31 +0200

    Updates

---
+++ precache-manifest.171c0ba9fdcacbdbb767ce179d907cf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fe028a51f013785e2d6",
+    "url": "/ethereal-pragma/static/js/runtime~main.1fe028a5.js"
+  },
+  {
+    "revision": "3bd30b659147187eee94",
+    "url": "/ethereal-pragma/static/js/main.3bd30b65.chunk.js"
+  },
+  {
+    "revision": "15c97f535dfd0dbc50cf",
+    "url": "/ethereal-pragma/static/js/1.15c97f53.chunk.js"
+  },
+  {
+    "revision": "3bd30b659147187eee94",
+    "url": "/ethereal-pragma/static/css/main.0ff40f3b.chunk.css"
+  },
+  {
+    "revision": "daa86dd0477edf50d85b6ecf8f41a0cd",
+    "url": "/ethereal-pragma/index.html"
+  }
+];
